17d6621168e43c09bc459a6a3425e6bf07e91194
Carlos Martínez <ced.mendoza@gmail.com>
2021-09-10 22:15:23 -0500

    Cleanup scripts

--- Player.gd
+++ Player.gd
@@ -1,5 +1,9 @@
 extends Spatial
 
+export(float) var vertical_sensibility = 0.1
+export(float) var horizontal_sensibility = 0.1
+export(float) var vertical_rotation_limit = 89
+
 onready var camera = $Camera
 
 func _ready():
@@ -7,10 +11,10 @@ func _ready():
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		var coords = event.get_relative()
-		rotate_y(-coords.x * 0.01)
-		camera.rotate_x(-coords.y * 0.01)
-		camera.rotation.x = clamp(camera.rotation.x, deg2rad(-89), deg2rad(89))
+		var coords = -event.get_relative()
+		rotate_y(deg2rad(coords.x * horizontal_sensibility))
+		camera.rotate_x(deg2rad(coords.y * vertical_sensibility))
+		camera.rotation.x = clamp(camera.rotation.x, deg2rad(-vertical_rotation_limit), deg2rad(vertical_rotation_limit))
 
 	if event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
