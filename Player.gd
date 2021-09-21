f422adda4b2bcc3c33acf636e33d9e49d9b3f2f8
Carlos Martínez <ced.mendoza@gmail.com>
2021-09-20 21:01:41 -0500

    Add gravity and jump to player (also add directional light!)

--- Player.gd
+++ Player.gd
@@ -4,8 +4,12 @@ export(float) var vertical_sensibility = 0.1
 export(float) var horizontal_sensibility = 0.1
 export(float) var vertical_rotation_limit = 89
 export(float) var walking_speed = 10
+export(float) var gravity = 9.8
+export(float) var jump_power = 8.0
 
 var velocity = Vector3.ZERO
+var gravity_vec = Vector3.ZERO
+var snap = Vector3.ZERO
 
 onready var camera = $Camera
 
@@ -23,7 +27,7 @@ func _input(event):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	var input_strength : Vector2 = Vector2.ZERO
 	input_strength.x = (
 		Input.get_action_strength("right") -
@@ -39,5 +43,16 @@ func _physics_process(_delta):
 	direction += -global_transform.basis.z * input_strength.y
 	direction = direction.normalized()
 	
+	if is_on_floor():
+		gravity_vec = Vector3.ZERO
+		snap = -get_floor_normal()
+	else:
+		gravity_vec += Vector3.DOWN * gravity * delta
+		snap = Vector3.DOWN
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		gravity_vec = Vector3.UP * jump_power
+		snap = Vector3.ZERO
+	
 	velocity = direction * walking_speed
-	velocity = move_and_slide(velocity, Vector3.UP)
+	velocity = move_and_slide_with_snap(velocity + gravity_vec, snap, Vector3.UP)
